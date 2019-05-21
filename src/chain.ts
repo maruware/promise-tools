3fdabc2f5bffaf602ba05a6659b33627beaddcbc
maruware <me@maruware.com>
2019-05-21 12:37:48 +0900

    remove log

--- src/chain.ts
+++ src/chain.ts
@@ -3,7 +3,6 @@ export function chain<T>(initialValue: T, catchCallback?: (e: Error) => void) {
   return {
     add(next: (prevVal: T) => Promise<T>) {
       last = last.then(next, e => {
-        console.log('e', e)
         catchCallback && catchCallback(e)
         return next(null)
       })
