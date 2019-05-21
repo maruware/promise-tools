d1dd00e89686bb9eab9addc31d6d13c8c5490f5c
maruware <me@maruware.com>
2019-05-21 12:02:28 +0900

    add test for chain catchCallback

--- __tests__/chain_test.ts
+++ __tests__/chain_test.ts
@@ -11,4 +11,16 @@ describe('chain test', () => {
 
     expect(last).toBe('abc')
   })
+
+  it('catchCallback should work', async () => {
+    const cb = jest.fn()
+    const q = chain('', cb)
+    q.add(async () => 'a')
+    q.add(() => Promise.reject(new Error('failed')))
+    await q.add(async () => 'b')
+
+    expect(cb.mock.calls.length).toBe(1)
+    const e = cb.mock.calls[0][0] as Error
+    expect(e.message).toBe('failed')
+  })
 })
--- src/chain.ts
+++ src/chain.ts
@@ -1,12 +1,13 @@
 export function chain<T>(initialValue: T, catchCallback?: (e: Error) => void) {
-  let prev = Promise.resolve(initialValue)
+  let last = Promise.resolve(initialValue)
   return {
     add(next: (prevVal: T) => Promise<T>) {
-      prev = prev.then(next).catch(e => {
+      last = last.then(next, e => {
+        console.log('e', e)
         catchCallback && catchCallback(e)
         return next(null)
       })
-      return prev
+      return last
     }
   }
 }
